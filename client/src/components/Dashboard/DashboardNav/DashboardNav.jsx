d352f2e9fc82164d2ff4757277b52c1e7be9522c
Rahul Ravindran <beingcodr@gmail.com>
2020-07-10 21:25:33 +0530

    Updated DashboardNav component with proper logic for .active class

--- client/src/components/Dashboard/DashboardNav/DashboardNav.jsx
+++ client/src/components/Dashboard/DashboardNav/DashboardNav.jsx
@@ -1,26 +1,17 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // SCSS files
 import './DashboardNav.scss';
-
-const NavItems = ({ link, linkName }) => {
-    const [activeState, setActiveState] = useState(false);
-    return (
-        <Link sytle={{ color: activeState && '#7ee3d9' }} to={link}>
-            {linkName}
-        </Link>
-    );
-};
 
 const DashboardNav = () => {
     const [activeItem, setAciveItem] = useState(null);
     return (
         <div id='dashboardNav'>
-            <NavItems link='/profile' linkName='profile' />
-            <NavItems link='/bookings' linkName='bookings' />
-            <NavItems link='/settings' linkName='settings' />
-            <NavItems link='/logout' linkName='logout' />
+            <NavLink to='/profile'>profile</NavLink>
+            <NavLink to='/bookings'>bookings</NavLink>
+            <NavLink to='/settings'>settings</NavLink>
+            <NavLink to='/logout'>logout</NavLink>
         </div>
     );
 };
