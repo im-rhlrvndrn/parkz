bc55ee70cba5094b362cb7f15a4e77af26a115c3
Rahul Ravindran <rahulr1116@gmail.com>
2020-07-23 17:57:10 +0530

    Adding responsiveness to the existing layouts

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Home = (props) => {
-    return <div>this is home page</div>;
+    return (
+        <div>
+            this is home page. Go to <NavLink to={`/user/im_rhlrvndrn`}>dashboard</NavLink>
+        </div>
+    );
 };
 
 export default Home;
